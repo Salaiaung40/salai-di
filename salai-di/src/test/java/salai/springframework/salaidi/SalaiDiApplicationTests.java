31b762699c5c12ecb59d8665a345493575adbe92
Salaiaung40 <salaihtunlin@hotnail.com>
2021-05-21 13:53:56 +0200

    Add Spring Project

---
+++ salai-di/src/test/java/salai/springframework/salaidi/SalaiDiApplicationTests.java
@@ -0,0 +1,13 @@
+package salai.springframework.salaidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaiDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/salai/springframework/salaidi/SalaiDiApplication.java
@@ -0,0 +1,13 @@
+package salai.springframework.salaidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaiDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaiDiApplication.class, args);
+	}
+
+}
